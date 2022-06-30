6d5d7aaf33c425438040553ba9ae639fa9fc98d9
SmOke11345 <103870835+SmOke11345@users.noreply.github.com>
2022-06-30 12:51:53 +0600

    Update Main.js

--- js/Main.js
+++ js/Main.js
@@ -4,23 +4,25 @@ $('.owl-carousel').owlCarousel({
     loop: true,
     dots: true,
     margin:50,
-    items:2,
+    items:1,
     autoplay:true,
     autoplayTimeout:5000,
     smartSpeed:300,
     responsive: {
-        420: {
-            items: 1
-        },
-        768: {
-            items: 1
+        1920: {
+            items: 2
         },
         1280: {
             items: 2
         },
-        1920: {
-            items: 2
-        }
+        768: {
+            items: 1
+        },
+        420: {
+            
+            items: 1
+        } 
+        
     }
 })
 
